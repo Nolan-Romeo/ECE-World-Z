c90fba166794e60c4c89be1358faf7c0ca3cdd67
Mattymacouille <matteo.devaux01@gmail.com>
2023-05-03 14:36:04 +0200

    animation taupes

--- src/jeu.c
+++ src/jeu.c
@@ -14,12 +14,14 @@ int main()
     ALLEGRO_TIMER* timer = al_create_timer(1.0 / 60.0);
     ALLEGRO_EVENT_QUEUE* queue = al_create_event_queue();
     ALLEGRO_DISPLAY* fenetre = al_create_display(1920, 1080);
-    ALLEGRO_FONT* font = al_create_builtin_font();
+
+
 
     al_register_event_source(queue, al_get_keyboard_event_source());
     al_register_event_source(queue, al_get_mouse_event_source());
     al_register_event_source(queue, al_get_display_event_source(fenetre));
     al_register_event_source(queue, al_get_timer_event_source(timer));
+
 
 
     ALLEGRO_EVENT event;
@@ -28,117 +30,97 @@ int main()
     ALLEGRO_BITMAP*fond = load_image("img/background.bmp");
     ALLEGRO_BITMAP*taupe_img =load_image("img/taupe.bmp");
     ALLEGRO_BITMAP*marteau = load_image("img/hammer.bmp");
-
+    //ALLEGRO_BITMAP*ecriture = load_image("img/things.png");
+    //ALLEGRO_BITMAP*nombre = load_image("img/nb.png");
+    ALLEGRO_FONT *font = al_create_builtin_font();
     ALLEGRO_MOUSE_CURSOR*curseur = al_create_mouse_cursor(marteau,0,0);
 
     al_set_mouse_cursor(fenetre,curseur);
 
-    Taupe taupe[5] = { 30, 650, 4, 0,
-                       520, 650, 4, 0,
-                       850, 650, 4, 0,
-                       1150, 650, 4, 0,
-                       1600, 650, 4, 0
-                    };
+    Taupe taupe[5] = { {30, 650, 0},
+                       {520, 650, 0},
+                       {850, 650, 0},
+                       {1150, 650, 0},
+                       {1600, 650, 0}
+                    }; 
 
     al_start_timer(timer);
-
-
     
     bool redraw=true;
     int fin=0;
     int frame_visible=0;
+    int random = 0;
+    int score=0;
+    int limit_random = getRandomInt(120,180);
 
     while(!fin){
 
-        al_wait_for_event(queue, &event);
+        if(!al_is_event_queue_empty(queue)){
+            al_get_next_event(queue, &event);
+            switch (event.type)
+            {
+            case ALLEGRO_EVENT_TIMER:
+                gererTaupes(taupe);
+                redraw = true;
+                break;
+            case ALLEGRO_EVENT_KEY_DOWN:
+                switch (event.keyboard.keycode)
+                {
+                case ALLEGRO_KEY_ESCAPE:
+                    al_destroy_display(fenetre);
+                    al_destroy_event_queue(queue);
+                    return 0;   
+                default:
+                    break;
+                }
+            case ALLEGRO_EVENT_MOUSE_BUTTON_DOWN:
+                if (event.mouse.button == 1) { // Clic gauche
+                    int x = event.mouse.x;
+                    int y = event.mouse.y;
+                    for(int i=0; i <5 ; i++){
+                        if(taupe[i].visible){
+                            if (x >= taupe[i].position_x && x <= taupe[i].position_x+103  && y>= 650 && y <= 650+97){
+                                taupe[i].visible=0;
+                                score=score+1;
+                            }
+                        }
+                    }
+                    
+                }
+                break;
 
-
-
-
-
-
-
-
-        
-
-         if (event.type == ALLEGRO_EVENT_TIMER && al_is_event_queue_empty(queue)) {
-            redraw=true;
-        } else if (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
-            fin = 1;
-        }else if(event.type == ALLEGRO_EVENT_KEY_DOWN){
-            ALLEGRO_KEYBOARD_STATE keyboard_state;
-            al_get_keyboard_state(&keyboard_state);
-            if(al_key_down(&keyboard_state, ALLEGRO_KEY_ESCAPE)) fin=1;
-            // else if(al_key_down(&keyboard_state, ALLEGRO_KEY_SPACE)) taupe->statut--;
+            default:
+                break;
+            }
         }
+
         if(redraw){
-
-
-            al_draw_scaled_bitmap(fond,0,0,1920,1080,0,0,1920,1080,0);
-/*             if(changePosition == getRandomInt(30,60)){
-                
-                changePosition = 0;
+            if(random == limit_random){
+                limit_random = getRandomInt(600,800);
+                random = 0;
+                random_taupe(taupe);
+            }else{
+                random++;
             }
 
-            if(changePosition %10 == 0){
-                for (int i = 0; i < 6; i++)
-                {
-                    if(taupe[i].visible && taupe[i].statut > 0) taupe[i].statut -= 1;
-                    if(taupe[i].statut == 0) taupe[i].statut=4;
-                }     
-            }*/
+            al_draw_scaled_bitmap(fond,0,0,1920,1080,0,0,1920,1080,0);
+            al_draw_textf(font, al_map_rgb(255, 255, 255), 100, 100, ALLEGRO_ALIGN_CENTER, "Mon score : %d", score);
 
-            random_taupe(taupe);
-            afficher_taupe(taupe,taupe_img,&frame_visible);
+            //al_draw_scaled_bitmap(ecriture,0,0,800,172,0,0,800,172,0);
+            //al_draw_tinted_scaled_rotated_bitmap_region(nombre, 0+44*(score), 0, 103, 100, al_map_rgb(255,255,255), 0, 0, 750, 20, 2, 2, 0, 0);
 
- /*      for (int j = 0; j < 5; j++)
-            {
-                int i = getRandomInt(0,5);
-                
-                if(taupe[i].visible){
-                    taupe[i].statut = taupe[i].statut-1;
-                     al_draw_tinted_scaled_rotated_bitmap_region(taupe_img, 103*(frame4), 97, 103, 100, al_map_rgb(255,255,255), 0, 0, taupe[i].position_x, taupe[i].position_y, 3, 3, 0, 0);}
-                
-                else{
-                    taupe[i].visible=1;
-                }
-            }*/
+            afficher_taupe(taupe,taupe_img,&frame_visible);          
 
-
-            /* int compare = 1;
-            for (int j = 0; j < 5; j++)
-            {   
-                int i=0;
-                while(compare != taupe[i].visible){
-                    i++;
-                }
-                compare++;
-                taupe[i].statut = changePosition/20;
-            } */
-
-            /* for(int i = 0; i < 5; i++){
-                if((double)changePosition-(taupe[i].visible*60) <= 0){
-                    taupe[i].statut = 4;
-                }else {
-                    taupe[i].statut = 4-changePosition/60.0-taupe[i].visible;
-                }
-                //taupe[i].statut = 4;
-            } */
-            
-            
-          
             al_flip_display();
         }
     }
 
     al_destroy_bitmap(marteau);
     al_destroy_mouse_cursor(curseur);
-    al_destroy_font(font);
-    al_destroy_display(fenetre);
     al_destroy_timer(timer);
-    al_destroy_event_queue(queue);
     al_destroy_bitmap(fond);
     al_destroy_bitmap(taupe_img);
-
+    //al_destroy_bitmap(ecriture);
+    al_destroy_font(font);
 
     }
